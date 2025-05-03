35a5410eeed3952f1bf268caf821e2fe33a6c745
Osama139 <osamaismail1399@gmail.com>
2025-05-03 17:05:23 +0500

    adding first github action

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,12 +1,30 @@
 import pytest
-from jose import jwt
-from app import schemas
-from app.config import settings
+
+from app import models, schemas
 
 
 def test_get_all_posts(authorized_client, test_posts):
     response = authorized_client.get("/posts")
-    print(response.json())
     assert response.status_code == 200
+
+def test_get_a_post(authorized_client, test_posts):
+    response = authorized_client.get("/posts/1")
+    assert response.status_code == 200
+
+@pytest.mark.parametrize("title, content, published", [
+    ("some title1", "some content1", True),
+    ("some title2", "some content2", True),
+    ("some title33", "some content3", False),
+])
+def test_create_post(authorized_client, test_user, title, content, published):
+    response = authorized_client.post('/posts', json={
+                                                      "title":title,
+                                                      "content":content,
+                                                      "published":published
+                                                      }
+                                      )
+    new_post = schemas.PostOut(**response.json())
+    assert new_post.user_id == test_user['id']
+    assert response.status_code == 201
 
 
